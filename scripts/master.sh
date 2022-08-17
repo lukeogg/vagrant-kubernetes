8d1f19d0c302d51936956a03cc95a91b26f2b688
Luke Ogg <lukeogg@gmail.com>
2022-08-17 15:23:44 -0600

    push diferent cidr for calico that doesn't conflict with home network

--- scripts/master.sh
+++ scripts/master.sh
@@ -6,7 +6,7 @@ set -euxo pipefail
 
 MASTER_IP="10.0.0.10"
 NODENAME=$(hostname -s)
-POD_CIDR="192.168.0.0/16"
+export POD_CIDR="172.20.0.0/16"
 
 sudo kubeadm config images pull
 
@@ -37,46 +37,10 @@ chmod +x /vagrant/configs/join.sh
 kubeadm token create --print-join-command > /vagrant/configs/join.sh
 
 # Install Calico Network Plugin
+curl https://github.com/lukeogg/vagrant-kubernetes/blob/cf32a74eaa3e80747cd7680aec3f4f8d3a8ae073/scripts/calico.yaml -O
 
-curl https://docs.projectcalico.org/manifests/calico.yaml -O
+envsubst "$POD_CIDR" < calico.yaml | kubectl apply -f -
 
-kubectl apply -f calico.yaml
-
-# Install Metrics Server
-
-# kubectl apply -f https://raw.githubusercontent.com/scriptcamp/kubeadm-scripts/main/manifests/metrics-server.yaml
-# kubectl apply -f configs/metrics-server.yaml
-
-# Install Kubernetes Dashboard (Instlaled with kommander)
-
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.1/aio/deploy/recommended.yaml
-
-# Create Dashboard User
-
-# cat <<EOF | kubectl apply -f -
-# apiVersion: v1
-# kind: ServiceAccount
-# metadata:
-#   name: admin-user
-#   namespace: kubernetes-dashboard
-# EOF
-
-# cat <<EOF | kubectl apply -f -
-# apiVersion: rbac.authorization.k8s.io/v1
-# kind: ClusterRoleBinding
-# metadata:
-#   name: admin-user
-# roleRef:
-#   apiGroup: rbac.authorization.k8s.io
-#   kind: ClusterRole
-#   name: cluster-admin
-# subjects:
-# - kind: ServiceAccount
-#   name: admin-user
-#   namespace: kubernetes-dashboard
-# EOF
-
-# kubectl -n kubernetes-dashboard get secret "$(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}")" -o go-template="{{.data.token | base64decode}}" >> /vagrant/configs/token
 
 sudo -i -u vagrant bash << EOF
 whoami
