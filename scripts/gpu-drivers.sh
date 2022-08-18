400561fe9958cf7299bd11489e99212cb998b6e1
Luke Ogg <lukeogg@gmail.com>
2022-08-17 22:24:16 -0600

    GPU CUDA drivers

--- scripts/gpu-drivers.sh
+++ scripts/gpu-drivers.sh
@@ -11,4 +11,9 @@ dpkg -i cuda-keyring_1.0-1_all.deb
 apt-get update
 apt-get -y install cuda-drivers
 
-nvidia-smi
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+apt-get update
+apt-get install -y nvidia-container-runtime
